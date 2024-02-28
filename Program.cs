cc89bec6b4787cde2c77a74eda9a1d1f2fe768e3
Shweta Vinod Kulkarni <shwetavinod@usf.edu>
2024-02-28 14:30:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿int[] selection_sort_numbers = { 3, 62, 5, 16, 4, 10 };
+Console.WriteLine("Here is the original array that will be used to execute the Selection Sort Algorithm: ");
+for (int i = 0; i < selection_sort_numbers.Length; i++)
+{
+    Console.Write("  " + selection_sort_numbers[i]);
+}
+Console.WriteLine("");
+Console.WriteLine("Here is the array after the execution of the Selection Sort Algorithm: ");
+Selection_Sort(selection_sort_numbers);
+Console.WriteLine("");
+static void Selection_Sort(int[] ss_arr)
+{
+    int min_position;
+    int temp;
+
+    for (int i = 0; i < ss_arr.Length; i++)
+    {
+        min_position = i;
+        for (int x = i + 1; x < ss_arr.Length; x++)
+        {
+            if (ss_arr[x] < ss_arr[min_position])
+            {
+                min_position = x;
+            }
+        } 
+
+        if (min_position != i)
+        {
+            temp = ss_arr[i];
+            ss_arr[i] = ss_arr[min_position];
+            ss_arr[min_position] = temp;
+        }
+        Console.Write("  " + ss_arr[i]);
+    } 
+}
